751d46d0b7b64c54f917c42c6e13d11a8e043acd
/paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-09-15 10:10:33 +0700

    init project

---
+++ app/src/main/java/com/ln/toptop/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ln.toptop.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ln.toptop.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
